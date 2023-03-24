3dfe9f97a939ef745f292f60318cfca6dd69e7c0
Srblaze1 <carloseduardocunharibeiro7@outlook.com>
2023-03-24 18:04:22 -0300

    new file:   AtividadePratica-S4/Imagens/Logo.png 	new file:   AtividadePratica-S4/Imagens/logo.svg 	new file:   AtividadePratica-S4/Imagens/logo_nav_bar.svg 	new file:   AtividadePratica-S4/Index/Pagina_Inicial.html 	new file:   AtividadePratica-S4/Index/css/animacao.css 	new file:   AtividadePratica-S4/Index/css/nav_bar.css 	new file:   AtividadePratica-S4/Index/css/rodape.css 	new file:   AtividadePratica-S4/Index/css/texto.css 	new file:   AtividadePratica-S4/Index/mobile-navbar.js 	new file:   AtividadePratica-S4/Sobre/index.html 	new file:   AtividadePratica-S4/inventario/index.css 	new file:   AtividadePratica-S4/inventario/index.html 	new file:   AtividadePratica-S4/inventario/invetario.js 	new file:   AtividadePratica-S4/login/login.css 	new file:   AtividadePratica-S4/login/login.html

---
+++ AtividadePratica-S4/inventario/invetario.js
@@ -0,0 +1,39 @@
+let inventario = [];
+
+function adicionarBem() {
+  let nome = document.getElementById("nome").value;
+  let quantidade = document.getElementById("quantidade").value;
+
+  if (nome !== "" && quantidade !== "") {
+    let novoBem = {
+      nome: nome,
+      quantidade: quantidade
+    };
+    inventario.push(novoBem);
+    atualizarTabela();
+    limparFormulario();
+  }
+}
+
+function atualizarTabela() {
+  let tabela = document.getElementById("inventario");
+
+  // limpar a tabela
+  while (tabela.rows.length > 1) {
+    tabela.deleteRow(-1);
+  }
+
+  // adicionar os bens à tabela
+  for (let i = 0; i < inventario.length; i++) {
+    let linha = tabela.insertRow(-1);
+    let nomeCelula = linha.insertCell(0);
+    let quantidadeCelula = linha.insertCell(1);
+    nomeCelula.innerHTML = inventario[i].nome;
+    quantidadeCelula.innerHTML = inventario[i].quantidade;
+  }
+}
+
+function limparFormulario() {
+  document.getElementById("nome").value = "";
+  document.getElementById("quantidade").value = "";
+}
