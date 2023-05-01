6b7b00d53056fa776f80edaa22c505ed535f915e
Srblaze1 <carloseduardocunharibeiro7@outlook.com>
2023-05-01 15:59:59 -0300

    modificação

---
+++ Semana 10/app.js
@@ -0,0 +1,66 @@
+/* Curso de Engenharia de Software - UniEVANGÉLICA 
+Disciplina de Programação Web 
+Dev: Carlos Eduardo Cunha Ribeiro
+DATA: 01/05/2023 */
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let inventory = [
+  { id: 1, name: 'TV', quantity: 2 },
+  { id: 2, name: 'Mesa', quantity: 1 },
+];
+
+// CREATE
+app.post('/api/inventory', (req, res) => {
+  const { name, quantity } = req.body;
+  const id = inventory.length + 1;
+  const item = { id, name, quantity };
+  inventory.push(item);
+  res.send({ message: 'Item adicionado com sucesso!', inputs: item });
+});
+
+// READ ALL
+app.get('/api/inventory', (req, res) => {
+  res.send(inventory);
+});
+
+// READ ONE
+app.get('/api/inventory/:id', (req, res) => {
+  const { id } = req.params;
+  const item = inventory.find((item) => item.id === parseInt(id));
+  if (!item) return res.status(404).send({ message: 'Item não encontrado' });
+  res.send(item);
+});
+
+// UPDATE
+app.put('/api/inventory/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, quantity } = req.body;
+  const item = inventory.find((item) => item.id === parseInt(id));
+  if (!item) return res.status(404).send({ message: 'Item não encontrado' });
+  item.name = name;
+  item.quantity = quantity;
+  res.send({ message: 'Item atualizado com sucesso!', inputs: item });
+});
+
+// DELETE
+app.delete('/api/inventory/:id', (req, res) => {
+  const { id } = req.params;
+  const item = inventory.find((item) => item.id === parseInt(id));
+  if (!item) return res.status(404).send({ message: 'Item não encontrado' });
+  const index = inventory.indexOf(item);
+  inventory.splice(index, 1);
+  res.send({ message: 'Item excluído com sucesso!', inputs: item });
+});
+
+app.get('/', (req, res) => {
+  res.send('Bem-vindo ao inventário familiar!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000!');
+});
